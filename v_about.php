501b0f4c1ed35455b864873f951f2df24c03aacd
MuhammadChandraDarmawan <mchandradrmwn12@gmail.com>
2021-10-10 05:19:32 -0700

    upload file template file sederhana

---
+++ v_about.php
@@ -0,0 +1,26 @@
+<section>
+	<h1><?= $judul; ?></h1>
+
+	<h4>Nama</h4>
+	<ul type="disc">
+		<li>Nama Depan : Chandra</li>
+		<li>Nama Belakang : Darmawan</li>
+	</ul>
+	<br>
+
+	<h4>Alamat</h4>
+	<ul type="none">
+		<li>Tajur Bogor Timur</li>
+	</ul>
+
+	<h4>Tempat Lahir</h4>
+	<ul type="none">
+		<li>Purworejo</li>
+	</ul>
+
+	<h4>OlahRaga Favorit</h4>
+	<ul type="square">
+		<li>taekwondo</li>
+	 	<li>Gym</li>
+	</ul>
+</section>
